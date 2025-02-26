e20944153d31c7888a6e038e095e799af5a480ce
jamess-nguyen <haimenguyen@gmail.com>
2025-02-25 19:52:47 -0800

    :sparkles: it curls the espn api's it's really swag

--- mlb_parser.py
+++ mlb_parser.py
@@ -1,7 +1,40 @@
+import sys
 import requests
 import json
 
-def fetch_scoreboard(date):
+
+def fetch_runs(game_id):
+    """
+
+    Accepts a specific mlb game ID and curls the game data
+    You can review example data at:
+    https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/summary?event=401570673
+
+    """
+
+    url = f"https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/summary?event={game_id}"
+    response = requests.get(url)
+    game_dict = json.loads(response.content)
+    
+    for team in game_dict["boxscore"]["teams"]:
+        if team["team"]["abbreviation"]!="LAA":
+            continue
+        if team["team"]["abbreviation"]=="LAA":
+            for stats in team["statistics"][0]["stats"]:
+                if stats["name"]=="runs":
+                    print(f"The Angels scored {stats['value']} runs at home")
+                    print("WEDIDITTT!!")
+
+
+def fetch_mlb_date(date):
+    """
+
+    Accepts a date and checks if the Angels played at home that day
+    If they played at home it'll search for the game data otherwise it'll just do nothing
+    You can review example data at:
+    https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard?dates=20240913
+
+    """
     url = f"https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard?dates={date}"
     response = requests.get(url)
     
@@ -14,22 +47,16 @@ def fetch_scoreboard(date):
 
     # Search
     mlb_dict = json.loads(response.content)
-    counter=False
-    for key, val in mlb_dict.items():
-        print(key)
-        print(type(val), len(val))
-        for i in val:
-            for key2, val2 in i.items():
-                if key2=="id":
-                    cur_id = val2
-                if key2=="shortName" and  " @ LAA" in val2:
-                    print(val2, cur_id)
-    
-    print(len(mlb_dict))
+    for key, events in mlb_dict.items():
+        if key != "events":
+            continue
+        for event in events:
+            cur_game_id = event.get("id")
+            cur_short_name = event.get("shortName")
+            if cur_short_name and " @ LAA" in cur_short_name:
+                fetch_runs(cur_game_id)
+                break
 
-# Check the scoreboard for a specific date
-fetch_scoreboard("20240913")
-
-# url = f"https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/summary?event=401570673"
-# response = requests.get(url)
-# print(response.content)
+if __name__ == "__main__":
+    date = sys.stdin.read().strip()
+    fetch_mlb_date(date)
