290418653face45c09f78112346a839a02084d7a
jamess-nguyen <haimenguyen@gmail.com>
2025-02-25 18:41:58 -0800

    :sparkles: found the endpoints we need

--- mlb_parser.py
+++ mlb_parser.py
@@ -1,17 +1,35 @@
 import requests
-from datetime import datetime
+import json
 
+def fetch_scoreboard(date):
+    url = f"https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard?dates={date}"
+    response = requests.get(url)
+    
+    # Check validity 
+    if response.status_code<200 or response.status_code>300:
+        raise LookupError
 
-cur_date_str = datetime.now().strftime('%Y%m%d')
-url = f"https://www.espn.com/mlb/scoreboard/_/date/{cur_date_str}"
+    if "@ LAA" not in str(response.content):
+        raise LookupError
 
-# Test URL
-url = "https://www.espn.com/mlb/scoreboard/_/date/20230627"
+    # Search
+    mlb_dict = json.loads(response.content)
+    counter=False
+    for key, val in mlb_dict.items():
+        print(key)
+        print(type(val), len(val))
+        for i in val:
+            for key2, val2 in i.items():
+                if key2=="id":
+                    cur_id = val2
+                if key2=="shortName" and  " @ LAA" in val2:
+                    print(val2, cur_id)
+    
+    print(len(mlb_dict))
 
-headers = {
-    'User-Agent': 'Safari/537.36'
-}
+# Check the scoreboard for a specific date
+fetch_scoreboard("20240913")
 
-response = requests.get(url, headers=headers)
-
-print(response.text)
+# url = f"https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/summary?event=401570673"
+# response = requests.get(url)
+# print(response.content)
